09e8ee3fb9ba93b951340f2ee0c0b19a1081ef4f
dean <deanfernandes18@gmail.com>
2025-01-16 16:41:47 +0000

    Add vs sln & winforms proj

---
+++ NorthfieldLibrary.LibraryManagementSystem/NorthfieldLibrary.LibraryManagementSystem/Form1.cs
@@ -0,0 +1,10 @@
+namespace NorthfieldLibrary.LibraryManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
