7974372ec9b1a4b01fd5b8c4afc3a4d39563bf05
dean <deanfernandes18@gmail.com>
2025-01-20 16:13:30 +0000

    Add App.config to gitignore; Implement LoginForm

--- Northfield.LibraryManagementSystem/Northfield.LibraryManagementSystem/LoginForm.cs
+++ Northfield.LibraryManagementSystem/Northfield.LibraryManagementSystem/LoginForm.cs
@@ -1,3 +1,6 @@
+using Northfield.LibraryManagementSystem.Library.Services;
+using System.Configuration;
+
 namespace Northfield.LibraryManagementSystem
 {
     public partial class LoginForm : Form
@@ -11,6 +14,45 @@ namespace Northfield.LibraryManagementSystem
             txtUsername.Left = (ClientSize.Width - txtUsername.Width) / 2;
             txtPassword.Left = (ClientSize.Width - txtPassword.Width) / 2;
             btnLogin.Left = (ClientSize.Width - btnLogin.Width) / 2;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUsername.Text) || string.IsNullOrEmpty(txtPassword.Text))
+            {
+                MessageBox.Show("Empty Username or Password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            SqlDatabaseService sqlDatabaseService = new SqlDatabaseService(ConfigurationManager.ConnectionStrings["default"].ConnectionString);
+            var admin = sqlDatabaseService.SelectAdmin(txtUsername.Text);
+
+            if (admin != null)
+            {
+                if (PasswordService.VerifyPassword(txtPassword.Text, admin.PasswordHash, admin.Salt))
+                {
+                    Hide();
+                    new DashboardForm(this).Show();
+
+                    return;
+                }
+            }
+
+            MessageBox.Show("Invalid Username or Password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            BeginInvoke(() => txtUsername.Focus());
+        }
+
+        private void txtPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnLogin_Click(sender, e);
+            }
         }
     }
 }
